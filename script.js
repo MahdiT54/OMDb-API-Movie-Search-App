688cfd8e76c6ee31779b9ee5f1d5a27c75477394
Mahdi T <mahditanzim2@gmail.com>
2023-06-25 00:36:30 -0400

    fixed query search

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function loadMovieDetails() {
       searchList.classList.add("hide-search-list");
       movieSearchBox.value = "";
       const result = await fetch(
-        `http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=1c44ead`
+        `https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=1c44ead`
       );
       const movieDetails = await result.json();
       // console.log(movieDetails);
