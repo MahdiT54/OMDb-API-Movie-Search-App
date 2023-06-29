6c6fdb11d2bc9a96715a1b9cc9802678685dd5cf
Mahdi T <mahditanzim2@gmail.com>
2023-06-29 19:04:32 -0400

    finished mahdiized layout

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 const movieSearchBox = document.getElementById("movie-search-box");
 const searchList = document.getElementById("search-list");
 const resultGrid = document.getElementById("result-grid");
+const filterToggle = document.getElementById("filter");
 
 // load movies from API
 async function loadMovies(searchTerm) {
@@ -18,9 +19,11 @@ function findMovies() {
   let searchTerm = movieSearchBox.value.trim();
   if (searchTerm.length > 0) {
     searchList.classList.remove("hide-search-list");
+    filterToggle.classList.remove("hide-search-list");
     loadMovies(searchTerm);
   } else {
     searchList.classList.add("hide-search-list");
+    filterToggle.classList.add("hide-search-list");
   }
 }
 
@@ -34,7 +37,7 @@ function displayMovieList(movies) {
     if (movies[idx].Poster !== "N/A") {
       moviePoster = movies[idx].Poster;
     } else {
-      moviePoster = "image_not_found.png";
+      moviePoster = "./assets/image_not_found.png";
     }
     movieListItem.innerHTML = `
     <div class="search-item-thumbnail">
@@ -56,6 +59,7 @@ function loadMovieDetails() {
     movie.addEventListener("click", async () => {
       // console.log(movie.dataset.id);
       searchList.classList.add("hide-search-list");
+      filterToggle.classList.add("hide-search-list");
       movieSearchBox.value = "";
       const result = await fetch(
         `https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=1c44ead`
@@ -71,7 +75,7 @@ function displayMovieDetails(details) {
   resultGrid.innerHTML = `
     <div class="movie-poster">
     <img src="${
-      details.Poster !== "N/A" ? details.Poster : "image_not_found.png"
+      details.Poster !== "N/A" ? details.Poster : "./assets/image_not_found.png"
     }" alt="movie poster" />
     </div>
     <div class="movie-info">
@@ -99,8 +103,10 @@ function displayMovieDetails(details) {
     `;
 }
 
-window.addEventListener("click", (event) => {
-  if (event.target.className !== "form-control") {
-    searchList.classList.add("hide-search-list");
-  }
-});
+// window.addEventListener("click", (event) => {
+//   if (event.target.className !== "form-control") {
+//     searchList.classList.add("hide-search-list");
+//     // findMovies()
+//     // filterToggle.classList.add("hide-search-list");
+//   }
+// });
